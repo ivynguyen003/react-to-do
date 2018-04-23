003b89d0adcf0f6ad20d94709f5569c03d6240c2
Ivy Nguyen <ivynguyen003@gmail.com>
2018-04-23 12:17:49 -0700

    add initial state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import './App.css';
 import ToDo from './components/toDo.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ul>
-          <ToDo />
-          <ToDo />
-        </ul>
-      </div>
-    );
+
+  class App extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        todos:[
+          { description:'Walk the dog', isCompleted: true},
+          { description:'Eat Breakfast', isCompleted: false },
+          { description:'Water lawn', isCompleted: false }
+        ]
+      };
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <ul>
+            <ToDo />
+            <ToDo />
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
