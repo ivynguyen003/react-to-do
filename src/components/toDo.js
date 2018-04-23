416608f2e70fe2341ec4ab0bc01067121568868b
Ivy Nguyen <ivynguyen003@gmail.com>
2018-04-23 09:52:34 -0700

    add toDo component and add import to App.js

---
+++ src/components/toDo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class ToDo extends Component { //is component a reset name in react?
+  render(){
+  	return (
+  		<li>1st to do item</li>
+  	);
+  }
+}
+
+export default ToDo;
