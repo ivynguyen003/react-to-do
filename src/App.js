4de16bf1a1df0bf378fa1e441f6bf266009feb8b
Ivy Nguyen <ivynguyen003@gmail.com>
2018-04-23 12:38:42 -0700

    render todo

--- src/App.js
+++ src/App.js
@@ -4,28 +4,27 @@ import ToDo from './components/toDo.js';
 
 class App extends Component {
 
-  class App extends Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        todos:[
-          { description:'Walk the dog', isCompleted: true},
-          { description:'Eat Breakfast', isCompleted: false },
-          { description:'Water lawn', isCompleted: false }
-        ]
-      };
-    }
+  constructor(props){
+    super(props);
+    this.state = {
+      todos:[
+        { description:'Walk the dog', isCompleted: true},
+        { description:'Eat Breakfast', isCompleted: false },
+        { description:'Water lawn', isCompleted: false }
+      ]
+    };
+  }
 
-    render() {
-      return (
-        <div className="App">
-          <ul>
-            <ToDo />
-            <ToDo />
-          </ul>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.todos.map((todo,index) => //evaluate the code as JS
+            <ToDo key={ index } />//assign the unique key to each child of the todos array
+          )}
+        </ul>
+      </div>
+    );
   }
 }
 
