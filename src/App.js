bcc69fae8b6d5d7c81a72c98b2b4fce849cee043
Ivy Nguyen <ivynguyen003@gmail.com>
2018-04-25 13:32:10 -0700

    add handleSubmit and handleChange

--- src/App.js
+++ src/App.js
@@ -11,12 +11,27 @@ class App extends Component {
         { description:'Walk the dog', isCompleted: true},
         { description:'Eat Breakfast', isCompleted: false },
         { description:'Water lawn', isCompleted: false }
-      ]
+      ],
+      newTodoDescription:''
     };
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    if(!this.state.newTodoDescription) { return }
+    const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+    this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: ''});
+  }
+
+  handleChange(e) {
+    this.setState({ newTodoDescription: e.target.value });
+  }
+
   toggleComplete(index) {
-    console.log(index);
+    const todos = this.state.todos.slice();//make a copy of the array
+    const todo = todos[index]; // assign unique indext to each todo item 
+    todo.isCompleted = todo.isCompleted ? false : true;// switch the checkbox
+    this.setState({ todos: todos });//Pass new state object 1st todos is a name, 2nd todos is the value,
   }
 
   render() {
@@ -26,6 +41,10 @@ class App extends Component {
           {this.state.todos.map((todo,index) => //evaluate the code as JS
             <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) }/>
           )}
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) }/>
+          <input type="submit" />
+        </form>
         </ul>
       </div>
     );
